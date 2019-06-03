342552b79fd49a563f88ce1df1d9dc58190b4ef1
key <412391882@qq.com>
2019-06-03 22:16:28 +0800

    add "add All_IP"
    add "add All_IP"

--- src/burp/Utilities.java
+++ src/burp/Utilities.java
@@ -1,51 +1,44 @@
 package burp;
 
-
-import java.io.PrintWriter;
+import java.io.*;
 import java.util.*;
 
-public class Utilities {
-	
-    public static byte[] addIPHead(IExtensionHelpers helpers, IHttpRequestResponse requestResponse,String key) {
-    	Map<String,String> map = new HashMap<String,String>();
-    	map.put("X-Forwarded-For","X-Forwarded-For: 127.0.0.1");
-    	map.put("X-Forwarded-Host","X-Forwarded-Host: 127.0.0.1");
-    	map.put("X-remote-IP","X-remote-IP: 127.0.0.1");
-    	map.put("X-Originating-IP","X-Originating-IP: 127.0.0.1");
-    	map.put("X-remote-addr","X-remote-addr: 127.0.0.1");
-    	map.put("True-Client-IP","True-Client-IP: 127.0.0.1");
-    	map.put("Client-IP","Client-IP: 127.0.0.1");
-    	map.put("X-Client-IP","X-Client-IP: 127.0.0.1");
-    	map.put("X-Real-IP","X-Real-IP: 127.0.0.1");
-   
-    	byte[] request = requestResponse.getRequest();
-        IRequestInfo requestInfo = helpers.analyzeRequest(request);
-        int bodyOffset = requestInfo.getBodyOffset();
-        String body = new String(request, bodyOffset, request.length - bodyOffset);
-        
+public class Utilities
+{
+    public static byte[] addIPHead(final IExtensionHelpers helpers, final IHttpRequestResponse requestResponse, final String key) {
+        final Map<String, String> map = new HashMap<String, String>();
+        map.put("X-Forwarded-For", "X-Forwarded-For: 127.0.0.1");
+        map.put("X-Forwarded-Host", "X-Forwarded-Host: 127.0.0.1");
+        map.put("X-remote-IP", "X-remote-IP: 127.0.0.1");
+        map.put("X-Originating-IP", "X-Originating-IP: 127.0.0.1");
+        map.put("X-remote-addr", "X-remote-addr: 127.0.0.1");
+        map.put("True-Client-IP", "True-Client-IP: 127.0.0.1");
+        map.put("Client-IP", "Client-IP: 127.0.0.1");
+        map.put("X-Client-IP", "X-Client-IP: 127.0.0.1");
+        map.put("X-Real-IP", "X-Real-IP: 127.0.0.1");
+        map.put("AllIP", "X-Forwarded-For: 127.0.0.1\r\nX-Forwarded-Host: 127.0.0.1\r\nX-remote-IP: 127.0.0.1\r\nX-Originating-IP: 127.0.0.1\r\nX-remote-addr: 127.0.0.1\r\nTrue-Client-IP: 127.0.0.1\r\nClient-IP: 127.0.0.1\r\nX-Client-IP: 127.0.0.1\r\nX-Real-IP: 127.0.0.1");
+        final byte[] request = requestResponse.getRequest();
+        final IRequestInfo requestInfo = helpers.analyzeRequest(request);
+        final int bodyOffset = requestInfo.getBodyOffset();
+        final String body = new String(request, bodyOffset, request.length - bodyOffset);
         Boolean isExists = false;
-        List<String> headers;
-        headers = helpers.analyzeRequest(request).getHeaders();
-        
-        for(int i = 0;i<headers.size();i++){
-        	if(headers.get(i).startsWith(key)){
-        		headers.set(i, map.get(key)); 
-        		isExists = true;
-        	}
+        final List<String> headers = helpers.analyzeRequest(request).getHeaders();
+        for (int i = 0; i < headers.size(); ++i) {
+            if (headers.get(i).startsWith(key)) {
+                headers.set(i, map.get(key));
+                isExists = true;
+            }
         }
-        if(!isExists){
-        	headers.add(map.get(key));
+        if (!isExists) {
+            headers.add(map.get(key));
         }
-        
         return helpers.buildHttpMessage(headers, body.getBytes());
     }
     
-
-    
-    public static byte[] modifyUserAgent(IExtensionHelpers helpers, IHttpRequestResponse requestResponse,String uaid) {
-    	Map<String,String> map = new HashMap<String,String>();
-    	map.put("Nexus7", "Mozilla/5.0 (Linux; Android 4.1.1; Nexus 7 Build/JRO03D) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.166  Safari/535.19");
-    	map.put("AndroidGalaxyS3", "Mozilla/5.0 (Linux; U; Android 4.0.4; en-gb; GT-I9300 Build/IMM76D) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30");
+    public static byte[] modifyUserAgent(final IExtensionHelpers helpers, final IHttpRequestResponse requestResponse, final String uaid) {
+        final Map<String, String> map = new HashMap<String, String>();
+        map.put("Nexus7", "Mozilla/5.0 (Linux; Android 4.1.1; Nexus 7 Build/JRO03D) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.166  Safari/535.19");
+        map.put("AndroidGalaxyS3", "Mozilla/5.0 (Linux; U; Android 4.0.4; en-gb; GT-I9300 Build/IMM76D) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30");
         map.put("AndroidGalaxyTab", "Mozilla/5.0 (Linux; U; Android 2.2; en-gb; GT-P1000 Build/FROYO) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1");
         map.put("FFAndroidHandset", "Mozilla/5.0 (Android; Mobile; rv:14.0) Gecko/14.0 Firefox/14.0");
         map.put("FFAndroidTablet", "Mozilla/5.0 (Android; Tablet; rv:14.0) Gecko/14.0 Firefox/14.0");
@@ -77,124 +70,113 @@ public class Utilities {
         map.put("Slurp", "Mozilla/5.0 (compatible; Yahoo! Slurp; http://help.yahoo.com/help/us/ysearch/slurp)");
         map.put("Baidu_pc", "Mozilla/5.0 (compatible; Baiduspider/2.0; +http://www.baidu.com/search/spider.html)");
         map.put("Baidu_app", "Mozilla/5.0 (Linux;u;Android 4.2.2;zh-cn;) AppleWebKit/534.46 (KHTML,like Gecko) Version/5.1 Mobile Safari/10600.6.3 (compatible; Baiduspider/2.0; +http://www.baidu.com/search/spider.html)");
-    	map.put("Win7Phone","Mozilla/4.0 (compatible; MSIE 7.0; Windows Phone OS 7.0; Trident/3.1; IEMobile/7.0; LG; GW910)");
-    	map.put("Win75Phone","Mozilla/5.0 (compatible; MSIE 9.0; Windows Phone OS 7.5; Trident/5.0; IEMobile/9.0; SAMSUNG; SGH-i917)");
-    	map.put("Win8Phone", "Mozilla/5.0 (compatible; MSIE 10.0; Windows Phone 8.0; Trident/6.0; IEMobile/10.0; ARM; Touch; NOKIA; Lumia 920)");
-    	map.put("iPad","Mozilla/5.0 (iPad; CPU OS 8_1_3 like Mac OS X) AppleWebKit/600.1.4 (KHTML, like Gecko) Version/8.0 Mobile/12B466 Safari/600.1.4");
-    	map.put("iPhone","Mozilla/5.0 (iPhone; CPU iPhone OS 8_0_2 like Mac OS X) AppleWebKit/600.1.4 (KHTML, like Gecko) Version/8.0 Mobile/12A366 Safari/600.1.4");
-    	map.put("iPod", "Mozilla/5.0 (iPod; CPU iPhone OS 5_1_1 like Mac OS X) AppleWebKit/534.46 (KHTML, like Gecko) Version/5.1 Mobile/9B206 Safari/7534.48.3");
-    	byte[] request = requestResponse.getRequest();
-        IRequestInfo requestInfo = helpers.analyzeRequest(request);
-        int bodyOffset = requestInfo.getBodyOffset();
-        String body = new String(request, bodyOffset, request.length - bodyOffset);
-
+        map.put("Win7Phone", "Mozilla/4.0 (compatible; MSIE 7.0; Windows Phone OS 7.0; Trident/3.1; IEMobile/7.0; LG; GW910)");
+        map.put("Win75Phone", "Mozilla/5.0 (compatible; MSIE 9.0; Windows Phone OS 7.5; Trident/5.0; IEMobile/9.0; SAMSUNG; SGH-i917)");
+        map.put("Win8Phone", "Mozilla/5.0 (compatible; MSIE 10.0; Windows Phone 8.0; Trident/6.0; IEMobile/10.0; ARM; Touch; NOKIA; Lumia 920)");
+        map.put("iPad", "Mozilla/5.0 (iPad; CPU OS 8_1_3 like Mac OS X) AppleWebKit/600.1.4 (KHTML, like Gecko) Version/8.0 Mobile/12B466 Safari/600.1.4");
+        map.put("iPhone", "Mozilla/5.0 (iPhone; CPU iPhone OS 8_0_2 like Mac OS X) AppleWebKit/600.1.4 (KHTML, like Gecko) Version/8.0 Mobile/12A366 Safari/600.1.4");
+        map.put("iPod", "Mozilla/5.0 (iPod; CPU iPhone OS 5_1_1 like Mac OS X) AppleWebKit/534.46 (KHTML, like Gecko) Version/5.1 Mobile/9B206 Safari/7534.48.3");
+        final byte[] request = requestResponse.getRequest();
+        final IRequestInfo requestInfo = helpers.analyzeRequest(request);
+        final int bodyOffset = requestInfo.getBodyOffset();
+        final String body = new String(request, bodyOffset, request.length - bodyOffset);
         Boolean isExists = false;
-        List<String> headers;
-        headers = helpers.analyzeRequest(request).getHeaders();
-        
-        for(int i = 0;i<headers.size();i++){
-        	if(headers.get(i).startsWith("User-Agent")){
-        		String strUA = String.format("User-Agent: %s", map.get(uaid));
-        		headers.set(i, strUA); 
-        		isExists = true;
-        	}
+        final List<String> headers = helpers.analyzeRequest(request).getHeaders();
+        for (int i = 0; i < headers.size(); ++i) {
+            if (headers.get(i).startsWith("User-Agent")) {
+                final String strUA = String.format("User-Agent: %s", map.get(uaid));
+                headers.set(i, strUA);
+                isExists = true;
+            }
         }
-        if(!isExists){
-        	headers.add(String.format("User-Agent: %s", map.get(uaid)));
+        if (!isExists) {
+            headers.add(String.format("User-Agent: %s", map.get(uaid)));
         }
-        
         return helpers.buildHttpMessage(headers, body.getBytes());
     }
     
-    public static byte[] addOrigin(IExtensionHelpers helpers, IHttpRequestResponse requestResponse) {
-        IHttpService httpService = requestResponse.getHttpService();
+    public static byte[] addOrigin(final IExtensionHelpers helpers, final IHttpRequestResponse requestResponse) {
+        final IHttpService httpService = requestResponse.getHttpService();
         String url;
-        if(httpService.getProtocol().equals("https")){
-        	url = String.format("%s://%s",httpService.getProtocol(),httpService.getHost(),httpService.getPort());
-        }else{
-        	url = String.format("%s://%s:%s",httpService.getProtocol(),httpService.getHost(),httpService.getPort());
+        if (httpService.getProtocol().equals("https")) {
+            url = String.format("%s://%s", httpService.getProtocol(), httpService.getHost(), httpService.getPort());
         }
-        
-    	byte[] request = requestResponse.getRequest();
-        IRequestInfo requestInfo = helpers.analyzeRequest(request);
-        int bodyOffset = requestInfo.getBodyOffset();
-        String body = new String(request, bodyOffset, request.length - bodyOffset);
-
+        else {
+            url = String.format("%s://%s:%s", httpService.getProtocol(), httpService.getHost(), httpService.getPort());
+        }
+        final byte[] request = requestResponse.getRequest();
+        final IRequestInfo requestInfo = helpers.analyzeRequest(request);
+        final int bodyOffset = requestInfo.getBodyOffset();
+        final String body = new String(request, bodyOffset, request.length - bodyOffset);
         Boolean isExists = false;
-        List<String> headers;
-        headers = helpers.analyzeRequest(request).getHeaders();
-                
-        for(int i = 0;i<headers.size();i++){
-        	if(headers.get(i).startsWith("Origin:")){
-        		String strOrigin = String.format("Origin: %s",url);
-        		headers.set(i, strOrigin); 
-        		isExists = true;
-        	}
+        final List<String> headers = helpers.analyzeRequest(request).getHeaders();
+        for (int i = 0; i < headers.size(); ++i) {
+            if (headers.get(i).startsWith("Origin:")) {
+                final String strOrigin = String.format("Origin: %s", url);
+                headers.set(i, strOrigin);
+                isExists = true;
+            }
         }
-        if(!isExists){
-        	headers.add(String.format("Origin: %s",url));
+        if (!isExists) {
+            headers.add(String.format("Origin: %s", url));
         }
-        
         return helpers.buildHttpMessage(headers, body.getBytes());
     }
     
-    public static byte[] updateCookie(IBurpExtenderCallbacks callbacks,IExtensionHelpers helpers, IHttpRequestResponse requestResponse) {
-    	byte[] request = requestResponse.getRequest();
-    	PrintWriter stdout = new PrintWriter(callbacks.getStdout(),true);
-    	String shorturl = requestResponse.getHttpService().toString();
-    	stdout.println("[+] "+shorturl);
-		String latestCookie = getLatestCookieFromHistory(callbacks,helpers,shorturl);
-		stdout.println("[+] "+latestCookie);
-		IRequestInfo requestInfo = helpers.analyzeRequest(request);
-        int bodyOffset = requestInfo.getBodyOffset();
-        String body = new String(request, bodyOffset, request.length - bodyOffset);
-        
+    public static byte[] updateCookie(final IBurpExtenderCallbacks callbacks, final IExtensionHelpers helpers, final IHttpRequestResponse requestResponse) {
+        final byte[] request = requestResponse.getRequest();
+        final PrintWriter stdout = new PrintWriter(callbacks.getStdout(), true);
+        final String shorturl = requestResponse.getHttpService().toString();
+        stdout.println("[+] " + shorturl);
+        final String latestCookie = getLatestCookieFromHistory(callbacks, helpers, shorturl);
+        stdout.println("[+] " + latestCookie);
+        final IRequestInfo requestInfo = helpers.analyzeRequest(request);
+        final int bodyOffset = requestInfo.getBodyOffset();
+        final String body = new String(request, bodyOffset, request.length - bodyOffset);
         Boolean isExists = false;
-        List<String> headers;
-        headers = helpers.analyzeRequest(request).getHeaders();
-        
-        for(int i = 0;i<headers.size();i++){
-        	if(headers.get(i).toLowerCase().startsWith("cookie:")){
-        		headers.set(i, latestCookie); 
-        		isExists = true;
-        	}
+        final List<String> headers = helpers.analyzeRequest(request).getHeaders();
+        for (int i = 0; i < headers.size(); ++i) {
+            if (headers.get(i).toLowerCase().startsWith("cookie:")) {
+                headers.set(i, latestCookie);
+                isExists = true;
+            }
         }
-        if(!isExists){
-        	headers.add(latestCookie);
+        if (!isExists) {
+            headers.add(latestCookie);
         }
-        
         return helpers.buildHttpMessage(headers, body.getBytes());
     }
     
-    public static IHttpRequestResponse[] Reverse(IHttpRequestResponse[] input){
-	    for (int start = 0, end = input.length - 1; start < end; start++, end--) {
-	    	IHttpRequestResponse temp = input[end];
-	        input[end] = input[start];
-	        input[start] = temp;
-	    }
-	    return input;
-	}
+    public static IHttpRequestResponse[] Reverse(final IHttpRequestResponse[] input) {
+        for (int start = 0, end = input.length - 1; start < end; ++start, --end) {
+            final IHttpRequestResponse temp = input[end];
+            input[end] = input[start];
+            input[start] = temp;
+        }
+        return input;
+    }
     
-    public static String getLatestCookieFromHistory(IBurpExtenderCallbacks callbacks,IExtensionHelpers helpers,String shortUrl){
-
-    	IHttpRequestResponse[]  historyMessages = Reverse(callbacks.getProxyHistory());
-    	String lastestCookie =null;
-    	for (IHttpRequestResponse historyMessage:historyMessages) {
-    		IRequestInfo hisAnalyzedRequest = helpers.analyzeRequest(historyMessage);
-    		String hisShortUrl = historyMessage.getHttpService().toString();
-    		
-    		if (hisShortUrl.equals(shortUrl)) {
-    			List<String> hisHeaders = hisAnalyzedRequest.getHeaders();
-    			for (String hisHeader:hisHeaders) {
-    				if (hisHeader.toLowerCase().startsWith("cookie:")) {
-    					lastestCookie = hisHeader;
-            			if(lastestCookie != null) {
-            				return lastestCookie;
-            			}
-    				}
-    			}
-    		}
-    	}
-		return null;
-	}
+    public static String getLatestCookieFromHistory(final IBurpExtenderCallbacks callbacks, final IExtensionHelpers helpers, final String shortUrl) {
+        final IHttpRequestResponse[] historyMessages = Reverse(callbacks.getProxyHistory());
+        String lastestCookie = null;
+        IHttpRequestResponse[] array;
+        for (int length = (array = historyMessages).length, i = 0; i < length; ++i) {
+            final IHttpRequestResponse historyMessage = array[i];
+            final IRequestInfo hisAnalyzedRequest = helpers.analyzeRequest(historyMessage);
+            final String hisShortUrl = historyMessage.getHttpService().toString();
+            if (hisShortUrl.equals(shortUrl)) {
+                final List<String> hisHeaders = hisAnalyzedRequest.getHeaders();
+                for (final String hisHeader : hisHeaders) {
+                    if (hisHeader.toLowerCase().startsWith("cookie:")) {
+                        lastestCookie = hisHeader;
+                        if (lastestCookie != null) {
+                            return lastestCookie;
+                        }
+                        continue;
+                    }
+                }
+            }
+        }
+        return null;
+    }
 }
